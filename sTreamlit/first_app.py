8f4b31082dd6379cb12b5e280245b2000433ab8d
Barani7304 <113904486+Barani7304@users.noreply.github.com>
2023-12-22 12:49:31 +0530

    Add files via upload

---
+++ sTreamlit/first_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.title("Barani Ilango's Portfolio")
+    st.write("Aspiring Astronaut, Btech Mechanical Engineering")
+
+    st.header("About Me")
+    st.write("I am currently studying Btech Mechanical Engineering and aspiring to become an Astronaut.")
+
+    
+
+if __name__ == "__main__":
+    main()
